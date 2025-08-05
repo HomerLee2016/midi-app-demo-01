bcfbae49f3b322ac4e876892cd7e6f8ea146f1bc
Homer Lee <homer.lee@cdp.net>
2025-08-05 12:58:57 +0100

    More beats, and add BPM

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, send_file
-import io
 from module.midi_utils import create_midi_file
 from module.keyboard_utils import render_keyboard
 
@@ -7,12 +6,14 @@ app = Flask(__name__)
 
 # Notes C3 to C4 (MIDI numbers 48–60)
 NOTES = list(range(48, 61))
-BEATS = 4
+BEATS = 32
 
 @app.route('/')
 def index():
+    bpm = request.args.get('bpm', default=120, type=int)
     note_types = render_keyboard()
-    return render_template('index.html', note_types=note_types, beats=BEATS)
+    # Pass the actual Unicode character for quarter note (♩)
+    return render_template('index.html', note_types=note_types, beats=BEATS, beat_speed=bpm, bpm_label="\u2669")  # default BPM
 
 @app.route('/save', methods=['POST'])
 def save_midi():
